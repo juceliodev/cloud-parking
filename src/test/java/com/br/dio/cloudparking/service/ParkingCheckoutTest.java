71137d19ca4febb9a1a5cb892dd768157fa53cf0
jucelio <juceliodesa@gmail.com>
2022-08-09 14:14:08 -0300

    Adicionando Persistencia

---
+++ src/test/java/com/br/dio/cloudparking/service/ParkingCheckoutTest.java
@@ -0,0 +1,12 @@
+package com.br.dio.cloudparking.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkingCheckoutTest {
+
+    @Test
+    void getBill() {
+    }
+}
